e24c4b87236f173a607fd10d3c1ca70f4f51951c
Mohamad <m.h.gholikhah@gmail.com>
2022-11-07 16:25:03 +0330

    (for) ساختن جدول ضرب با استفاده از

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<table border="10">
+    <?php
+    for ($m = 1; $m <= 20; $m++) {
+        echo "<tr>";
+        for ($h = 1; $h <= 20; $h++) {
+            echo "<td>";
+            echo $m * $h;
+        }
+        echo "<br>";
+        echo "</tr>";
+    };
+    ?>
+</table>
+</body>
+</html>
+
+
